07b319f9d11ceb86b3af2227643689cfcc4a4788
ferswift <nandofhen@gmail.com>
2026-04-21 15:32:16 -0300

    Starting the project

---
+++ src/config/Firebase.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgjrohrKoKET2tZMjyx6CtzPbiCTjlqlI",
+  authDomain: "projeto-1-cbbfa.firebaseapp.com",
+  projectId: "projeto-1-cbbfa",
+  storageBucket: "projeto-1-cbbfa.firebasestorage.app",
+  messagingSenderId: "274869297506",
+  appId: "1:274869297506:web:3dd138fb74e02281474c3b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
